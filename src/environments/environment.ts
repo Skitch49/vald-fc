1937e4ed038aec196ffe7113f2aa23fe5e5e513f
Skitch49 <delaunay.alexis49@gmail.com>
2026-02-03 21:07:00 +0100

    Add environment configuration file and restore Thumbs.db entry in .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiValdUrl: 'https://api-vald.onrender.com',
+  redirectUri: 'https://vald-fc.netlify.app',
+  postLogoutRedirectUri: 'https://vald-fc.netlify.app',
+  recaptcha: '6LeqUTMqAAAAAC4KyhdkrKTk5QpgNvJMOj_7C6jX',
+};
